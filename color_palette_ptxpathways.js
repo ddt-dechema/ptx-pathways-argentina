5feb938a77a28940e5c39d548e628d8b8389d682
ddt-dechema <dinhdu.tran@dechema.de>
2023-09-15 17:43:46 +0200

    added more files for working first site

---
+++ color_palette_ptxpathways.js
@@ -0,0 +1,58 @@
+// circle border
+// biogas: #9
+// others: #10
+
+// PtX Pathways Corporate Design - Color Palette # 6
+// rgb(113, 184, 210)
+// Aluminium
+// Steel
+// Cement
+// Cellulose and paper
+
+// Palette # 8
+// rgb(164, 146, 220)
+// Thermal power plant
+// Refinery
+
+// Palette # 5
+// rgb(143, 196, 146)
+// Biogas Power Plant
+// Bioethanol
+
+// Palette # 3
+// rgb(241, 219, 38)
+// Ammonia
+// Methanol
+// Etileno
+
+let baseColors = {
+    "base_first": "rgb(0, 182, 157)",
+    "base_second": "rgb(191, 226, 71)",
+    "base_fourth": "rgb(255, 187, 0)"
+}, 
+emissionColors_D = {
+    // "CO2, AIR": 'rgb(241, 177, 48)',
+    // "CO, AIR": 'rgb(234,110,57)'
+    "biogas": "rgb(84, 114, 155)",
+    "industrial": "rgb(57, 137, 126)" 
+},
+emissionTypeColors_D = {
+    // "chemical parks": "rgb(0,168,189)",
+    // "polyol plants": "rgb(12,168,118)",
+    // "steel mills": "yellow"
+    "aluminium": "rgb(113, 184, 210, 1)",
+    "steel": "rgb(113, 184, 210, 0.6)",
+    "cement": "rgb(113, 184, 210, 0.4)",
+    "cellulose": "rgb(113, 184, 210, 0.2)",
+
+    "thermal": "rgb(164, 146, 220, 1)",
+    "refinery": "rgb(164, 146, 220, 0.6)",
+
+    "biogas": "rgb(143, 196, 146, 1)",
+    "bioethanol": "rgb(143, 196, 146, 0.6)",
+
+    "ammonia": "rgb(241, 219, 38, 1)",
+    "methanol": "rgb(241, 219, 38, 0.6)",
+    "etileno": "rgb(241, 219, 38, 0.4)"
+}
+
